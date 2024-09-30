c8b97534625ae09acc22d2abc9110740c9dfe381
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2024-09-30 15:33:34 -0700

    add logos, prizes

--- src/components/SponsorCard/SponsorCard.tsx
+++ src/components/SponsorCard/SponsorCard.tsx
@@ -4,13 +4,15 @@ import React from "react";
 import styles from "./SponsorCard.module.css";
 import Link from "@docusaurus/Link";
 
-const SponsorCard = ({ name, tracks, logo, link }) => {
+const SponsorCard = ({ name, track, logo, link }) => {
+  const trackValue = Array.isArray(track) ? track.join(", ") : track;
+
   return (
     <div className={styles.card}>
       <Link to={link} className={styles.link}>
         <img src={logo} alt={`${name} Logo`} className={styles.logo} />
         <h3 className={styles.name}>{name}</h3>
-        <p className={styles.tracks}>{tracks}</p>
+        <p className={styles.track}>{trackValue}</p>
       </Link>
     </div>
   );
