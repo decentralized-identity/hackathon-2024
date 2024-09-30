1d6cbe0e4bba521b582c7e3901b5e56744bb8f8e
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2024-09-29 17:45:57 -0700

    updates

--- src/components/SponsorCard/SponsorCard.tsx
+++ src/components/SponsorCard/SponsorCard.tsx
@@ -4,13 +4,13 @@ import React from "react";
 import styles from "./SponsorCard.module.css";
 import Link from "@docusaurus/Link";
 
-const SponsorCard = ({ name, description, logo, link }) => {
+const SponsorCard = ({ name, tracks, logo, link }) => {
   return (
     <div className={styles.card}>
       <Link to={link} className={styles.link}>
         <img src={logo} alt={`${name} Logo`} className={styles.logo} />
         <h3 className={styles.name}>{name}</h3>
-        <p className={styles.description}>{description}</p>
+        <p className={styles.tracks}>{tracks}</p>
       </Link>
     </div>
   );
